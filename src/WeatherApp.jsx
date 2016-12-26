8a81a1b87c7af1dd82c679098ad14816b79d9b2b
mschemmari <marianosche@gmail.com>
2016-12-26 13:36:21 -0300

    weather app rendered first element

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class WeatherApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Weather app will go here!</h2>
+      </div>
+    );
+  }
+}
